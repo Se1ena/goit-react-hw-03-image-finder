2e335a80f820a45a79104463792f83710293ae8c
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2023-01-29 14:53:26 +0100

    files update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Loader } from './Loader/Loader';
+import { Button } from './Button/Button';
+import { Modal } from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    search: '',
+    galleryItems: [],
+    modalImg: '',
+    page: 1,
+  };
+
+  handleFormSubmit = (value) => {
+    this.setState({
+      search: value.search,
+      galleryItems: [],
+      page: 1,
+    });
+  };
+
+  render (){
+    return (<Searchbar onSubmit={this.handleFormSubmit} />
+    
+    );
+  };
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import { BsSearch } from 'react-icons/bs';
+import { Search, SearchForm, SearchFormBtn, Input } from './Searchbar.styled';
+
+export const Searchbar = ({ onSubmit }) => {
+
+  const handleSubmit = (values, actions) => {
+    onSubmit(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Search>
+      <Formik initialValues={{ search: '' }} onSubmit={handleSubmit}>
+        <SearchForm>
+          <SearchFormBtn type="submit"></SearchFormBtn>
+          <BsSearch size="20px" />
+          <Input
+            name="search"
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </SearchForm>
+      </Formik>
+    </Search>
+  );
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
