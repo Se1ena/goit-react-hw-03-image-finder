427fc1ce1a5c3c4cc2e7cda0e8a7a604be8d3790
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2023-01-29 18:14:31 +0100

    files update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
+import { Wrapper } from './App.styled';
+
+import { fetchImages } from '../services/api-service';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class App extends Component {
   state = {
@@ -12,7 +16,40 @@ export class App extends Component {
     galleryItems: [],
     modalImg: '',
     page: 1,
+    status: 'start',
   };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { search, page } = this.state;
+    try {
+      if (prevState.search !== search || prevState.page !== page) {
+        this.setState({ status: 'loading' });
+        const res = await fetchImages(search, page);
+        if (res.data.total === 0) {
+          throw new Error('Images with your querry was not found');
+        }
+        this.setState((prevState) => (
+          {
+            galleryItems: [...prevState.galleryItems, ...this.getGalleryItems(res.data.hits)],
+            status: 'loaded',
+          }
+        ));
+      }
+    } catch (error) {
+      toast.error(error.message, {
+        position: 'top-center',
+        autoClose: 2500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+      this.setState({ status: 'start' });
+
+    }
+  }
 
   handleFormSubmit = (value) => {
     this.setState({
@@ -22,9 +59,29 @@ export class App extends Component {
     });
   };
 
-  render (){
-    return (<Searchbar onSubmit={this.handleFormSubmit} />
-    
-    );
+  loadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
+
+  handleModal = (image) => {
+    this.setState({ modalImg: image });
+  };
+
+  getGalleryItems = (data) => {
+    return data.map(el => ({
+      id: el.id, webformatURL: el.webformatURL, largeImageURL: el.largeImageURL,
+    }));
+  }
+
+  render() {
+    const { status, modalImg, galleryItems } = this.state;
+    return (<Wrapper>
+      <Searchbar onSubmit={this.handleFormSubmit} />
+      {galleryItems.length > 0 && <ImageGallery galleryItems={galleryItems} onClick={this.handleModal} />}
+      {status === 'loading' && <Loader />}
+      {status === 'loaded' && <Button loadMore={this.loadMore} />}
+      {modalImg && <Modal image={modalImg} onModalClose={this.handleModal} />}
+      <ToastContainer />
+    </Wrapper>);
+  }
 };
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Overlay, ModalWindow } from './Modal.styled';
+
+export class Modal extends Component {
+
+  static propTypes = {
+    image: PropTypes.string.isRequired,
+    onModalClose: PropTypes.func.isRequired,
+  };
+
+  closeModalOnEsc = e => {
+    if (e.code === 'Escape') {
+      this.props.onModalClose('');
+    }
+  };
+
+  handleBackDropClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onModalClose('');
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.closeModalOnEsc);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.closeModalOnEsc);
+  }
+
+  render() {
+    const { image } = this.props;
+    return (
+      <Overlay className='overlay' onClick={this.handleBackDropClick}>
+        <ModalWindow className='modal'>
+          <img src={image} alt='' />
+        </ModalWindow>
+      </Overlay>
+    );
+  }
+}
